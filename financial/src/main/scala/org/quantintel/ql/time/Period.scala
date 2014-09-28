e4d789d5d2ae7b930f52e7ca540203f472fa79ec
Paul Bernard <paul.bernard@teleapp.com>
2014-09-27 20:06:26 -0400

    refactored for consistency

--- financial/src/main/scala/org/quantintel/ql/time/Period.scala
+++ financial/src/main/scala/org/quantintel/ql/time/Period.scala
@@ -22,16 +22,19 @@ package org.quantintel.ql.time
 
 import org.quantintel.ql.time.Frequency._
 import org.quantintel.ql.time.TimeUnit.TimeUnit
+import org.quantintel.ql.time.TimeUnit._
 
+/**
+ * @author Paul Bernard
+ */
 object Period {
+
 
   val UNKNOWN_FREQUENCY = "unknown frequency"
   val UNKNOWN_TIME_UNIT = "unknown time unit"
   val INCOMPATIBLE_TIME_UNIT = "incompatible time unit"
   val UNDECIDABLE_COMPARISON = "undecidable comparison"
   val DIVISION_BY_ZERO_ERROR = "cannot be divided by zero"
-
-  import org.quantintel.ql.time.TimeUnit._
 
   val ONE_YEAR_FORWARD = new Period(1, YEARS)
   val ONE_YEAR_BACKWARD = new Period(-1, YEARS)
@@ -40,190 +43,82 @@ object Period {
   val ONE_DAY_FORWARD = new Period(1, DAYS)
   val ONE_DAY_BACKWARD = new Period(-1, DAYS)
 
-  def apply(length : Int, units : TimeUnit) = {
-      new Period(length, units)
+  def apply(l : Int, p: TimeUnit) : Period = {
+    new Period(l, p)
   }
 
-
-
-}
-
-import org.quantintel.ql.time.TimeUnit._
-
-/**
- * @author Paul Bernard
- */
-class Period (var length : Int, var units : TimeUnit)  {
-
-
-
-  import org.quantintel.ql.time.Period._
-
-  def this(f: Frequency)  {
-
-
-
-    this(
-      f match {
-        case ONCE | NO_FREQUENCY => 0
-        case ANNUAL =>  1
-        case SEMIANNUAL | EVERY_FOURTH_MONTH | QUARTERLY | BIMONTHLY | MONTHLY =>  12 / f.id
-        case EVERY_FOURTH_WEEK | BIWEEKLY | WEEKLY => 52 / f.id
-        case DAILY => 1
-        case _ => throw new Exception(Period.UNKNOWN_FREQUENCY)
-      } ,
-      f match {
-        case ONCE | NO_FREQUENCY => DAYS
-        case ANNUAL =>  YEARS
-        case SEMIANNUAL | EVERY_FOURTH_MONTH | QUARTERLY | BIMONTHLY | MONTHLY =>  MONTHS
-        case EVERY_FOURTH_WEEK | BIWEEKLY | WEEKLY => WEEKS
-        case DAILY => DAYS
-        case _ => throw new Exception(Period.UNKNOWN_FREQUENCY)
-      })
+  def negate (p: Period) : Period = {
+    Period(-p.length, p.units)
   }
 
-  override def clone: Period = Period(length, units)
+  def * (n: Int, p: Period) : Period = {
+    Period(n*p.length, p.units)
+  }
 
-  def negative = Period(-length, units)
+  def * (p: Period, n: Int) : Period = {
+    Period(n*p.length, p.units)
+  }
 
-  def addAssign(another: Period) : Period  = {
-    if (length == 0){
-      length = another.length
-      units = another.units
-    } else if (units == another.units){
-      length = length + another.length
-    } else {
-      this.units match {
-        case YEARS => {
-          another.units match {
-            case MONTHS => {
-              units = another.units
-              length = length*12 + another.length
-            }
-            case WEEKS | DAYS => throw new IllegalArgumentException(INCOMPATIBLE_TIME_UNIT)
-            case _ => throw new Exception(UNKNOWN_TIME_UNIT)
-          }
-        }
-        case MONTHS => {
-          another.units match {
-            case YEARS => {
-              length = length + another.length * 12
-            }
-            case WEEKS | DAYS => throw new IllegalArgumentException(INCOMPATIBLE_TIME_UNIT)
-            case _ => throw new Exception(UNKNOWN_TIME_UNIT)
-          }
-        }
-        case WEEKS => {
-          another.units match {
-            case DAYS => {
-              units = another.units
-              length = length*7 + another.length
-            }
-            case YEARS | MONTHS => throw new IllegalArgumentException(INCOMPATIBLE_TIME_UNIT)
-            case _ => throw new Exception(UNKNOWN_TIME_UNIT)
-          }
-        }
-        case DAYS => {
-          another.units match {
-            case WEEKS => {
-              length = length + another.length*7
-            }
-            case YEARS | MONTHS => throw new IllegalArgumentException(INCOMPATIBLE_TIME_UNIT)
-            case _ => throw new Exception(UNKNOWN_TIME_UNIT)
-          }
-        }
-        case _ => new Exception(UNKNOWN_TIME_UNIT)
-      }
-    }
+  def / (p: Period, n: Int) : Period = {
+    Period(p.length/n, p.units)
+  }
 
-    this
+  def / (n: Int, p: Period) : Period = {
+    Period(p.length/n, p.units)
+  }
+
+  def == (p1: Period, p2: Period) : Boolean = {
+   !(Period.<(p1,p2) || p2 < p1)
+  }
+
+  def !=(p1: Period, p2: Period) : Boolean = {
+    !(p1 == p2)
+  }
+
+  def >(p1: Period, p2: Period): Boolean = {
+    Period.<(p2,p1)
+  }
+
+  def <=(p1: Period, p2: Period) : Boolean = {
+    !(p1 > p2)
+  }
+
+  def >=(p1: Period, p2: Period) : Boolean = {
+    !(Period.<(p1,p2))
+  }
+
+  def + (p1: Period, p2: Period): Period = {
+    var result: Period = p1
+    result += p2
+    result
+  }
+
+  def - (p1: Period, p2: Period) : Period = {
+    var result : Period = p1
+    result -= p2
+    result
+  }
+
+  def < (p1: Period, p2: Period) : Boolean = {
+
+    if (p1.length == 0) return p2.length > 0
+    if (p2.length == 0) return p1.length < 0
+
+    if (p1.units == p2.units) return p1.length < p2.length
+    if (p1.units == MONTHS && p2.units == YEARS) return p1.length < 12 * p2.length
+    if (p1.units == YEARS && p2.units == MONTHS) return 12 * p1.length < p2.length
+    if (p1.units == DAYS && p2.units == WEEKS) return p1.length < 7*p2.length
+    if (p1.units == WEEKS && p2.units == DAYS) return 7*p1.length < p2.length
+
+    val p1lim : (Int, Int) = daysMinMax(p1)
+    val p2lim : (Int, Int) = daysMinMax(p2)
+
+    if (p1lim._2 < p2lim._1) return true
+    else if (p1lim._1 > p2lim._2) return false
+    else throw new Exception("undecidable comparison between " + p1 + " and " + p2)
 
   }
 
-  def subAssign(another: Period): Period = this.addAssign(another.clone.negative)
-
-  def divAssign(scalar: Int): Period = {
-    if (scalar == 0) throw new ArithmeticException(DIVISION_BY_ZERO_ERROR)
-
-    if (length % scalar == 0) length = length / scalar
-    else {
-      units match {
-        case YEARS => {
-          units = MONTHS
-          length = length * 12
-        }
-        case WEEKS => {
-          units = DAYS
-          length = length * 7
-        }
-      }
-    }
-
-    if (length % scalar == 0) length = length/scalar
-    else throw new Exception("cannot be divided by " + scalar)
-
-    this
-  }
-
-  def mul(scalar: Int): Period = Period(scalar * length, units)
-
-  def add(another: Period) : Period = clone.addAssign(another)
-
-  def sub(another: Period) : Period = clone.subAssign(another)
-
-  def div(scalar: Int) : Period = clone.divAssign(scalar)
-
-  def eq (another: Period): Boolean = equals(another)
-
-  def neg(another: Period): Boolean = !equals(another)
-
-  def gt(another: Period): Boolean = another.lt(this)
-
-  def le(another: Period): Boolean = lt(another) || eq(another)
-
-  def ge(another: Period): Boolean = another.le(this)
-
-  def lt(another: Period) : Boolean = {
-
-    if (units == another.units) return length < another.length
-    if (units == MONTHS && another.units == YEARS) return length < 12 * another.length
-    if (units == YEARS && another.units == MONTHS) return 12 * length < another.length
-    if (units == DAYS && another.units == WEEKS) return length < 7 * another.length
-    if (units == WEEKS && another.units == DAYS) return 7 * length < another.length
-
-    val period1MinDays = minDays
-    val period1MaxDays = maxDays
-    val period2MinDays = another.minDays
-    val period2MaxDays = another.maxDays
-
-    if (period1MaxDays < period2MinDays) true
-    else if (period1MinDays > period2MaxDays) false else throw new Exception(UNDECIDABLE_COMPARISON)
-
-
-  }
-
-  def frequency : Frequency = {
-
-    val length : Int = Math.abs(this.length)
-
-    if(length == 0) return NO_FREQUENCY
-
-    units match {
-      case YEARS => if (length == 1) return ANNUAL else OTHER_FREQUENCY
-      case MONTHS => if (12 % length == 0 && length <=12)
-          Frequency.valueOf(12 / length)
-        else OTHER_FREQUENCY
-      case WEEKS => {
-        if (length == 1) WEEKLY
-        else if (length == 2) BIWEEKLY
-        else if (length == 4) EVERY_FOURTH_WEEK
-        else OTHER_FREQUENCY
-      }
-      case DAYS => if (length ==1) DAILY else OTHER_FREQUENCY
-      case _ => throw new Exception(UNKNOWN_TIME_UNIT)
-    }
-
-  }
 
   def years(p: Period) : Double = {
 
@@ -272,22 +167,250 @@ class Period (var length : Int, var units : TimeUnit)  {
     }
   }
 
-  override def equals(obj : Any) : Boolean = {
+  private def daysMinMax(p: Period) : (Int, Int) = {
+    p match {
+      case DAYS => (p.length, p.length)
+      case WEEKS => (7*p.length, 7*p.length)
+      case MONTHS => (28*p.length, 31*p.length)
+      case YEARS => (365*p.length, 366*p.length)
+      case _ => throw new Exception("unknown time unit (" + p.units + ")")
+    }
+  }
 
-    if (obj == this) true
-    else if (obj == null) false
-    else if (!obj.isInstanceOf[Period]) false
-    else {
-      obj.asInstanceOf[Period].fEquals(this)
+
+
+}
+
+import org.quantintel.ql.time.TimeUnit._
+
+/**
+ * @author Paul Bernard
+ */
+class Period  {
+
+  var length: Int = 0
+  var units: TimeUnit = null
+
+
+  import org.quantintel.ql.time.Period._
+
+  def this(l : Int, u : TimeUnit) {
+    this()
+    length = l
+    units = u
+  }
+
+
+  def this(f: Frequency)  {
+    this()
+    f match {
+      case NO_FREQUENCY => {
+        units = DAYS
+        length = 0
+      }
+      case ONCE => {
+        units = YEARS
+        length = 0
+      }
+      case ANNUAL => {
+        units = YEARS
+        length = 1
+      }
+      case SEMIANNUAL | EVERY_FOURTH_MONTH | QUARTERLY | BIMONTHLY | MONTHLY => {
+        units = MONTHS
+        length = 12/f.id
+      }
+      case EVERY_FOURTH_WEEK | BIWEEKLY | WEEKLY => {
+        units = WEEKS
+        length = 52/f.id
+      }
+      case DAILY => {
+        units = DAYS
+        length = 1
+      }
+      case OTHER_FREQUENCY => throw new Exception("unknown frequency")
+      case _ => throw new Exception("unknown frequency")
+
+
     }
 
   }
 
-  def fEquals(period : Period) : Boolean = {
-    if (period.isInstanceOf[Period]) return false
-    if (length != period.asInstanceOf[Period].length) return false
-    if (units != period.asInstanceOf[Period].units) return false
-    true
+
+  def frequency : Frequency = {
+
+    val length : Int = this.length.abs
+
+    if (length==0){
+      if(units == YEARS) return ONCE else NO_FREQUENCY
+    }
+
+    units match {
+      case YEARS => if (length ==1) ANNUAL else OTHER_FREQUENCY
+      case MONTHS => if (12%length == 0 && length <=12)
+        Frequency.valueOf(12/length) else OTHER_FREQUENCY
+      case WEEKS => {
+        if (length == 1) WEEKLY
+        else if (length == 2) BIWEEKLY
+        else if (length == 4) EVERY_FOURTH_WEEK
+        else OTHER_FREQUENCY
+      }
+      case DAYS => if (length==1) DAILY else OTHER_FREQUENCY
+      case _ => throw new Exception("unknown time unit (" + units.id + ")")
+
+    }
+  }
+
+  def += (p: Period) = {
+
+    if (length == 0){
+      length = p.length
+      units = p.units
+    } else if (units == p.units){
+      length = length + p.length
+    } else {
+      this.units match {
+        case YEARS => {
+          p.units match {
+            case MONTHS => {
+              units = p.units
+              length = length*12 + p.length
+            }
+            case WEEKS | DAYS => throw new IllegalArgumentException(INCOMPATIBLE_TIME_UNIT)
+            case _ => throw new Exception(UNKNOWN_TIME_UNIT)
+          }
+        }
+        case MONTHS => {
+          p.units match {
+            case YEARS => {
+              length = length + p.length * 12
+            }
+            case WEEKS | DAYS => throw new IllegalArgumentException(INCOMPATIBLE_TIME_UNIT)
+            case _ => throw new Exception(UNKNOWN_TIME_UNIT)
+          }
+        }
+        case WEEKS => {
+          p.units match {
+            case DAYS => {
+              units = p.units
+              length = length*7 + p.length
+            }
+            case YEARS | MONTHS => throw new IllegalArgumentException(INCOMPATIBLE_TIME_UNIT)
+            case _ => throw new Exception(UNKNOWN_TIME_UNIT)
+          }
+        }
+        case DAYS => {
+          p.units match {
+            case WEEKS => {
+              length = length + p.length*7
+            }
+            case YEARS | MONTHS => throw new IllegalArgumentException(INCOMPATIBLE_TIME_UNIT)
+            case _ => throw new Exception(UNKNOWN_TIME_UNIT)
+          }
+        }
+        case _ => new Exception(UNKNOWN_TIME_UNIT)
+      }
+    }
+
+    this
+
+  }
+
+  def -=(p: Period): Period = {
+    this += (negate(p))
+  }
+
+  def /= (scalar: Int): Period = {
+
+    require(scalar != 0, "cannot be divided by zero")
+
+    if (length % scalar == 0) length = length / scalar
+    else {
+      var u : TimeUnit = units
+      var l : Int = length
+      u match {
+        case YEARS => {
+          u = MONTHS
+          l = l * 12
+        }
+        case WEEKS => {
+          u = DAYS
+          l = l * 7
+        }
+      }
+      require(l % scalar == 0, this + "cannot be divided by " + scalar)
+
+      length = l/scalar
+      units = u
+
+    }
+
+    this
+  }
+
+
+  def normalize: Unit = {
+    if(length!=0){
+      units match {
+        case DAYS => {
+            if (!((length%7)==0)) {
+              length /=7
+              units = WEEKS
+            }
+        }
+        case MONTHS => {
+            if (!(length%12==0)){
+              length /= 12
+              units = YEARS
+            }
+        }
+        case WEEKS => { // no op
+         }
+        case YEARS => { // no op
+         }
+        case _ => throw new Exception("unknown time unit (" + units.id + ")")
+      }
+    }
+  }
+
+  def >= (p2: Period) : Boolean = {
+    Period.>=(this, p2)
+  }
+
+  def <= (p2: Period) : Boolean = {
+    Period.<=(this, p2)
+  }
+
+  def < (p2: Period) : Boolean = {
+     Period.<(this, p2)
+  }
+
+  def > (p2: Period) : Boolean = {
+    Period.>(this, p2)
+  }
+
+  def == (p2: Period) : Boolean = {
+    Period.==(this, p2)
+  }
+
+  def != (p2: Period) : Boolean = {
+    Period.!=(this, p2)
+  }
+
+  def + (p2: Period) : Period = {
+    Period.+(this, p2)
+  }
+
+  def - (p2: Period) : Period = {
+    Period.-(this, p2)
+  }
+
+  def * (n: Int) : Period = {
+    Period.*(this, n)
+  }
+
+  def / (n: Int) : Period = {
+    Period./(this, n)
   }
 
 
@@ -302,7 +425,6 @@ class Period (var length : Int, var units : TimeUnit)  {
     }
   }
 
-
   private def maxDays : Int = {
     units match {
       case YEARS => length * 366
@@ -313,9 +435,6 @@ class Period (var length : Int, var units : TimeUnit)  {
     }
 
   }
-
-
-
 
 
 }
