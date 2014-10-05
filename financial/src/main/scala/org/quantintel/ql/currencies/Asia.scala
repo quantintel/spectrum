495eeef3262831cf93f0e143e91e331c54f7ff79
Paul Bernard <paul.bernard@teleapp.com>
2014-10-05 16:11:16 -0400

    Initial commit

--- financial/src/main/scala/org/quantintel/ql/currencies/Asia.scala
+++ financial/src/main/scala/org/quantintel/ql/currencies/Asia.scala
@@ -30,226 +30,230 @@ import org.quantintel.ql.math.Rounding
 
 // Asia
 
-/**
- * Description:             Bangladesh taka
- * ISO three-letter code:   BDT
- * Numerical Code:          50
- * Divided by:              100 paisa
- */
+class Asia {
 
-class BDTCurrency extends Currency {
+  /**
+   * Description:             Bangladesh taka
+   * ISO three-letter code:   BDT
+   * Numerical Code:          50
+   * Divided by:              100 paisa
+   */
 
-  val bdtData = Data("Bangladesh taka", "BDT", 50, "Bt", "", 100, Rounding(), "%3% %1$.2f")
+  class BDTCurrency extends Currency {
 
-  data = bdtData
+    val bdtData = Data("Bangladesh taka", "BDT", 50, "Bt", "", 100, Rounding(), "%3% %1$.2f")
 
-}
+    data = bdtData
 
-/**
- * Description:             Chinese yuan
- * ISO three-letter code:   CNY
- * Numerical Code:          156
- * Divided by:              100 fend
- */
-class CNYCurrency extends Currency {
+  }
 
-  val cnyData = Data("Chinese yuan", "CNY", 156, "Y", "", 100, Rounding(), "%3% %1$.2f")
+  /**
+   * Description:             Chinese yuan
+   * ISO three-letter code:   CNY
+   * Numerical Code:          156
+   * Divided by:              100 fend
+   */
+  class CNYCurrency extends Currency {
 
-  data = cnyData
+    val cnyData = Data("Chinese yuan", "CNY", 156, "Y", "", 100, Rounding(), "%3% %1$.2f")
 
-}
+    data = cnyData
 
-/**
- * Description:             Hong Kong Dollar
- * ISO three-letter code:   HKD
- * Numerical Code:          344
- * Divided by:              100 cents
- */
-class HKDCurrency extends Currency {
+  }
 
-  val hdkData = Data("Hong Kong dollar", "HKD", 344, "HK$", "", 100, Rounding(), "%3% %1$.2f" )
+  /**
+   * Description:             Hong Kong Dollar
+   * ISO three-letter code:   HKD
+   * Numerical Code:          344
+   * Divided by:              100 cents
+   */
+  class HKDCurrency extends Currency {
 
-  data = hdkData
+    val hdkData = Data("Hong Kong dollar", "HKD", 344, "HK$", "", 100, Rounding(), "%3% %1$.2f")
 
-}
+    data = hdkData
 
-/**
- * Description:             Israeli shekel
- * ISO three-letter code:   ILS
- * Numerical Code:          375
- * Divided by:              100 agorot
- */
-class ILSCurrency extends Currency {
+  }
 
-  val ilsData = Data("Israeli shekel", "ILS", 376, "NIS", "", 100, Rounding(), "%1% %2$.3f")
+  /**
+   * Description:             Israeli shekel
+   * ISO three-letter code:   ILS
+   * Numerical Code:          375
+   * Divided by:              100 agorot
+   */
+  class ILSCurrency extends Currency {
 
-  data = ilsData
-}
+    val ilsData = Data("Israeli shekel", "ILS", 376, "NIS", "", 100, Rounding(), "%1% %2$.3f")
 
-/**
- * Description:             Indian rupee
- * ISO three-letter code:   INR
- * Numerical Code:          356
- * Divided by:              100 paise
- */
-class INRCurrency extends Currency {
+    data = ilsData
+  }
 
-  val inrData = Data("Indian rupee", "INR", 356, "Rs", "", 100, Rounding(), "%3% %1$.2f")
+  /**
+   * Description:             Indian rupee
+   * ISO three-letter code:   INR
+   * Numerical Code:          356
+   * Divided by:              100 paise
+   */
+  class INRCurrency extends Currency {
 
-  data = inrData
+    val inrData = Data("Indian rupee", "INR", 356, "Rs", "", 100, Rounding(), "%3% %1$.2f")
 
-}
+    data = inrData
 
-/**
- * Description:             Iraqi dinar
- * ISO three-letter code:   IQD
- * Numerical Code:          368
- * Divided by:              100 fils
- */
-class IQDCurrency extends Currency {
+  }
 
-  val iqdData = Data("Iraqi dinar", "IQD", 368, "ID", "", 1000, Rounding(), "%2% %1$.3f")
+  /**
+   * Description:             Iraqi dinar
+   * ISO three-letter code:   IQD
+   * Numerical Code:          368
+   * Divided by:              100 fils
+   */
+  class IQDCurrency extends Currency {
 
-  data = iqdData
+    val iqdData = Data("Iraqi dinar", "IQD", 368, "ID", "", 1000, Rounding(), "%2% %1$.3f")
 
-}
+    data = iqdData
 
-/**
- * Description:             Iranian rial
- * ISO three-letter code:   IRR
- * Numerical Code:          364
- * Divided by:              1 (not otherwise sub-dividable)
- */
-class IRRCurrency extends Currency {
+  }
 
-  val irrData = Data("Iranian rail", "IRR", 364, "Rls", "", 1, Rounding(), "%3% %1$.2f")
+  /**
+   * Description:             Iranian rial
+   * ISO three-letter code:   IRR
+   * Numerical Code:          364
+   * Divided by:              1 (not otherwise sub-dividable)
+   */
+  class IRRCurrency extends Currency {
 
-  data = irrData
+    val irrData = Data("Iranian rail", "IRR", 364, "Rls", "", 1, Rounding(), "%3% %1$.2f")
 
-}
+    data = irrData
 
-/**
- * Description:             Japanese yet
- * ISO three-letter code:   JPY
- * Numerical Code:          392
- * Divided by:              100
- */
-class  YPYCurrency extends Currency {
+  }
 
-  val ypyData = Data("Japanese yen", "JPY", 392, "u00a5", "", 100, Rounding(), "%3% %1$.0f")
+  /**
+   * Description:             Japanese yet
+   * ISO three-letter code:   JPY
+   * Numerical Code:          392
+   * Divided by:              100
+   */
+  class YPYCurrency extends Currency {
 
-  data = ypyData
+    val ypyData = Data("Japanese yen", "JPY", 392, "u00a5", "", 100, Rounding(), "%3% %1$.0f")
 
-}
+    data = ypyData
 
-/**
- * Description:             South-Korean won
- * ISO three-letter code:   KRW
- * Numerical Code:          410
- * Divided by:              100 chon
- */
-class KRWCurrency extends Currency {
+  }
 
-  val krwData = Data("South-Korean won", "KRW", 410, "W", "", 100, Rounding(), "%3% %1$.0f")
+  /**
+   * Description:             South-Korean won
+   * ISO three-letter code:   KRW
+   * Numerical Code:          410
+   * Divided by:              100 chon
+   */
+  class KRWCurrency extends Currency {
 
-  data = krwData
+    val krwData = Data("South-Korean won", "KRW", 410, "W", "", 100, Rounding(), "%3% %1$.0f")
 
-}
+    data = krwData
 
-/**
- * Description:             Kuwaiti dinar
- * ISO three-letter code:   KWD
- * Numerical Code:          414
- * Divided by:              1000 fils
- */
-class KWDCurrency extends Currency {
+  }
 
-  val kwdData = Data("Kuwait dinar", "KWD", 414, "KD", "", 1000, Rounding(), "%3% %1$.3f")
+  /**
+   * Description:             Kuwaiti dinar
+   * ISO three-letter code:   KWD
+   * Numerical Code:          414
+   * Divided by:              1000 fils
+   */
+  class KWDCurrency extends Currency {
 
-  data = kwdData
+    val kwdData = Data("Kuwait dinar", "KWD", 414, "KD", "", 1000, Rounding(), "%3% %1$.3f")
 
-}
+    data = kwdData
 
-/**
- * Description:             Nepal rupee
- * ISO three-letter code:   NPR
- * Numerical Code:          524
- * Divided by:              100 paise
- */
-class NPRCurrency extends Currency {
+  }
 
-  val nprData = Data ("Nepal rupee", "NPR", 524, "NRs", "", 100, Rounding(), "%3% %1$.2f")
+  /**
+   * Description:             Nepal rupee
+   * ISO three-letter code:   NPR
+   * Numerical Code:          524
+   * Divided by:              100 paise
+   */
+  class NPRCurrency extends Currency {
 
-  data = nprData
+    val nprData = Data("Nepal rupee", "NPR", 524, "NRs", "", 100, Rounding(), "%3% %1$.2f")
 
-}
+    data = nprData
 
-/**
- * Description:             Pakistani rupee
- * ISO three-letter code:   PKR
- * Numerical Code:          586
- * Divided by:              100 paisa
- */
-class PKRCurrency extends Currency {
+  }
 
-  val pkrData = Data("Pakistani rupee", "PKR", 586, "Rs", "", 100, Rounding(), "%3% %1$.2f")
+  /**
+   * Description:             Pakistani rupee
+   * ISO three-letter code:   PKR
+   * Numerical Code:          586
+   * Divided by:              100 paisa
+   */
+  class PKRCurrency extends Currency {
 
-  data = pkrData
+    val pkrData = Data("Pakistani rupee", "PKR", 586, "Rs", "", 100, Rounding(), "%3% %1$.2f")
 
-}
+    data = pkrData
 
-/**
- * Description:             Saudi riyal
- * ISO three-letter code:   SAR
- * Numerical Code:          682
- * Divided by:              100 halalat
- */
-class SARCurrrency extends Currency {
+  }
 
-  val sarData = Data("Saudi riyal", "SAR",682,"SRls", "", 100, Rounding(), "%3% %1$.2f")
+  /**
+   * Description:             Saudi riyal
+   * ISO three-letter code:   SAR
+   * Numerical Code:          682
+   * Divided by:              100 halalat
+   */
+  class SARCurrrency extends Currency {
 
-  data = sarData
+    val sarData = Data("Saudi riyal", "SAR", 682, "SRls", "", 100, Rounding(), "%3% %1$.2f")
 
-}
+    data = sarData
 
-/**
- * Description:             Singapore Dollar
- * ISO three-letter code:   SGD
- * Numerical Code:          702
- * Divided by:              100 cents
- */
-class SGDCurrency extends Currency {
+  }
 
-  val sgdData = Data("Singapore dollar", "SGD", 702, "S$", "", 100, Rounding(), "%3% %1$.2f")
+  /**
+   * Description:             Singapore Dollar
+   * ISO three-letter code:   SGD
+   * Numerical Code:          702
+   * Divided by:              100 cents
+   */
+  class SGDCurrency extends Currency {
 
-  data = sgdData
+    val sgdData = Data("Singapore dollar", "SGD", 702, "S$", "", 100, Rounding(), "%3% %1$.2f")
 
-}
+    data = sgdData
 
-/**
- * Description:             Thai baht
- * ISO three-letter code:   THB
- * Numerical Code:          764
- * Divided by:              100 stang
- */
-class THBCurrency extends Currency {
+  }
 
-  val thbData = Data("Thai bhat", "THB", 764,"Bht", "", 100, Rounding(), "%1% %2$.3f")
+  /**
+   * Description:             Thai baht
+   * ISO three-letter code:   THB
+   * Numerical Code:          764
+   * Divided by:              100 stang
+   */
+  class THBCurrency extends Currency {
 
-  data = thbData
+    val thbData = Data("Thai bhat", "THB", 764, "Bht", "", 100, Rounding(), "%1% %2$.3f")
 
-}
+    data = thbData
 
-/**
- * Description:             Taiwan dollar
- * ISO three-letter code:   TWD
- * Numerical Code:          901
- * Divided by:              100 cents
- */
-class TWDCurrency extends Currency {
+  }
 
-  val twdData = Data("Taiwan dollar", "TWD", 901, "NT$", "", 100, Rounding(), "%3% %1$.2f")
+  /**
+   * Description:             Taiwan dollar
+   * ISO three-letter code:   TWD
+   * Numerical Code:          901
+   * Divided by:              100 cents
+   */
+  class TWDCurrency extends Currency {
 
-  data = twdData
+    val twdData = Data("Taiwan dollar", "TWD", 901, "NT$", "", 100, Rounding(), "%3% %1$.2f")
+
+    data = twdData
+
+  }
 
 }
