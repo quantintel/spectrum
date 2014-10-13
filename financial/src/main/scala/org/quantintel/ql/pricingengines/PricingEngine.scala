5af28b5bd015306e5ef89682b32a1a322860f370
Paul Bernard <paul.bernard@teleapp.com>
2014-10-12 21:50:19 -0400

    Initial commit

---
+++ financial/src/main/scala/org/quantintel/ql/pricingengines/PricingEngine.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2014  Paul Bernard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Spectrum Finance is based in part on:
+ *        QuantLib. http://quantlib.org/
+ *
+ */
+
+package org.quantintel.ql.pricingengines
+
+/**
+ * @author Paul Bernard
+ */
+trait PricingEngine {
+
+  val PRICING_ENGINE_NOT_SET = "Pricing engine not set"
+
+  def getArguments: Arguments
+
+  def getResults : Results
+
+  def reset
+
+  def calculate
+
+  def update
+
+}
+
+trait Arguments {
+  def validate
+}
+
+trait Results {
+  def reset
+}
