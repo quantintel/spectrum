7aea227b2c7decd9eaa8eef5e6a4dd682cdd559d
Paul Bernard <paul.bernard@teleapp.com>
2014-10-14 21:41:42 -0400

    Initial commit

---
+++ financial/src/test/scala/org/quantintel/ql/math/RoundingTest.scala
@@ -0,0 +1,81 @@
+package org.quantintel.ql.math
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+ * @author Paul Bernard
+ */
+class RoundingTest extends FlatSpec with Matchers {
+
+  "integer values" should "in correct sequence" in {
+    assert(RoundingMethods.NONE.id == 1)
+    assert(RoundingMethods.UP.id == 2)
+    assert(RoundingMethods.DOWN.id == 3)
+    assert(RoundingMethods.CLOSEST.id == 4)
+    assert(RoundingMethods.FLOOR.id == 5)
+    assert(RoundingMethods.CEILING.id == 6)
+  }
+
+  "integer valueOf" should "in correct sequence" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(RoundingMethods.valueOf(1) == NONE)
+    assert(RoundingMethods.valueOf(2) == UP)
+    assert(RoundingMethods.valueOf(3)== DOWN)
+    assert(RoundingMethods.valueOf(4) == CLOSEST)
+    assert(RoundingMethods.valueOf(5)== FLOOR)
+    assert(RoundingMethods.valueOf(6) == CEILING)
+  }
+
+  "No rounding" should "be no change" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding().round(50.12345)==50.12345)
+    assert(Rounding(NONE, 5).round(50.12345)==50.12345)
+  }
+
+  "DOWN should" should "truncate past the point of precision" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding(DOWN, 5).round(50.1234599)==50.12345)
+  }
+
+  "UP should" should "round up the digit after the point of precision 1" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding(UP, 5).round(50.1234599)==50.12346)
+  }
+
+  "UP should" should "round up the digit after the point of precision 2" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding(UP, 5).round(50.1234533)==50.12346)
+  }
+
+  "CLOSEST should" should "round up the digit after the point of precision 50.12345" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding(CLOSEST, 5).round(50.1234533)==50.12345)
+  }
+
+  "CLOSEST should" should "round up the digit after the point of precision 50.12346" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding(CLOSEST, 5).round(50.1234553)==50.12346)
+  }
+
+
+  "FLOOR should" should "round up the digit after the point of precision 50.12346" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding(FLOOR, 5).round(50.1234553)==50.12346)
+  }
+
+  "FLOOR should" should "round up the digit after the point of precision -50.12345" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding(FLOOR, 5).round(-50.1234553)== -50.12345)
+  }
+
+  "CEILING should" should "round up the digit after the point of precision -50.12346" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding(CEILING, 5).round(-50.1234553)== -50.12346)
+  }
+
+  "CEILING should" should "round up the digit after the point of precision -50.12345" in {
+    import org.quantintel.ql.math.RoundingMethods._
+    assert(Rounding(CEILING, 5).round(50.1234553)== 50.12345)
+  }
+
+}
