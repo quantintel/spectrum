03bdc03e4b65ed24d6cd89de1cd90556211a215b
Paul Bernard <paul.bernard@teleapp.com>
2014-10-14 21:43:06 -0400

    modval short circuit.

--- financial/src/main/scala/org/quantintel/ql/math/Rounding.scala
+++ financial/src/main/scala/org/quantintel/ql/math/Rounding.scala
@@ -20,36 +20,40 @@
 
 package org.quantintel.ql.math
 
-
+/**
+ *
+ * 1 = do not round: return the number unmodified
+ *
+ * 2 = the first decimal place past the precision will be
+ * rounded up.  This differs from the OMG rule which
+ * rounds up only if the decimal to be rounded is
+ * greater than or equal to the rounding digit
+ *
+ * 3 = All decimal places past the precision will be truncated
+ *
+ * 4 = The first decimal place past the precision
+ * will be rounded up if greater than or equal
+ * to the rounding digit; this corresponds
+ * to the OMG round-up rule.
+ *
+ * 5 = positive numbers will be rounded up and negative
+ * numbers will be rounded down using the OMG round up
+ * and round down rules
+ *
+ * 6 = positive numbers will be rounded down and negative
+ * numbers will be rounded up using the OMG round up
+ * and round down rules
+ */
 object RoundingMethods extends Enumeration  {
 
 
     type RoundingMethods = Value
     val NONE = Value(1)
-    // do not round: return the number unmodified
     val UP = Value(2)
-    /* the first decimal place past the precision will be
-                            rounded up.  This differs from the OMG rule which
-                            rounds up only if the decimal to be rounded is
-                            greater than or equal to the rounding digit */
     val DOWN = Value(3)
-    /* All decimal places past the precision will be
-                            truncated */
     val CLOSEST = Value(4)
-    /*The first decimal place past the precision
-                            will be rounded up if greater than or equal
-                            to the OMG round-up digit; this corresponds
-                            to the OMG round-up rule.  When the rounding
-                            digit is 5, the result will be the one
-                            closets to the original number, hence the name */
     val FLOOR = Value(5)
-    /* positive numbers will be rounded up and negative
-                            numbers will be rounded down using the OMG round up
-                            and round down rules */
     val CEILING = Value(6)
-    /* positive numbers will be rounded down and negative
-                            numbers will be rounded up using the OMG round up
-                            and round down rules */
 
     def valueOf(methods: Int) = methods match {
       case 1 => NONE
@@ -85,29 +89,20 @@ class Rounding {
 
   def round(value: Double) : Double = {
 
-    if (method == NONE) value
+    if (method == NONE) return value
 
     val mult: Double = Math.pow(10.0, precision)
-    val neg: Boolean = (value < 0.0)
+    val neg: Boolean = value < 0.0
     var lvalue : Double = Math.abs(value) * mult
-    var integral : Double = lvalue.asInstanceOf[Int]
-    val modVal : Double = (lvalue-lvalue.asInstanceOf[Int])
+    val modVal : Double = lvalue - lvalue.asInstanceOf[Int]
     lvalue = lvalue - modVal
 
     method match {
-      case DOWN => {
-         // no op
-      }
-      case UP => {
-        lvalue = lvalue + 1.0
-      }
-      case CLOSEST => {
-        if (modVal >= (digit/10.0)){
-          lvalue = lvalue + 1.0
-        }
-      }
+      case DOWN => { }
+      case UP => if (modVal != 0.0) lvalue = lvalue + 1.0
+      case CLOSEST => if (modVal >= (digit/10.0)) lvalue = lvalue + 1.0
       case FLOOR => {
-        if (!neg){
+        if (!neg) {
           if (modVal >= (digit/10.0)){
             lvalue = lvalue + 1.0
           }
@@ -120,13 +115,12 @@ class Rounding {
           }
         }
       }
-      case _ => {}
+      case _ => throw new Exception("unknown rounding method")
     }
 
     if (neg) -(lvalue/mult) else lvalue/mult
 
   }
-
 
 }
 
