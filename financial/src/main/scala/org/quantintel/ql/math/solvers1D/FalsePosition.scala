dbb6b5a7e1c761f16d41952b82cd7f78474a1cdf
Paul Bernard <paul.bernard@teleapp.com>
2014-11-02 20:56:26 -0500

    Initial commit

---
+++ financial/src/main/scala/org/quantintel/ql/math/solvers1D/FalsePosition.scala
@@ -0,0 +1,15 @@
+package org.quantintel.ql.math.solvers1D
+
+/**
+ * The false position method or regula falsi method is a term for propblem-
+ * solving methods in arithmetic, algebra, and calculus.  In simple terms
+ * these methods begin by attempting to evaluate a problem using test("false")
+ * values for the variables, and then adjust the values accordingly.
+ *
+ * @author Paul Bernard
+ */
+class FalsePosition {
+
+
+
+}
