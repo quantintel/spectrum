d5e7c1614a498a276f9f09ac3145940f79e37097
Paul Bernard <paul.bernard@teleapp.com>
2014-11-02 21:53:23 -0500

    Initial commit

--- financial/src/main/scala/org/quantintel/ql/math/optimization/NoConstraint.scala
+++ financial/src/main/scala/org/quantintel/ql/math/optimization/NoConstraint.scala
@@ -31,7 +31,7 @@ class NoConstraint extends Constraint {
 
 }
 
-class NoCostaintImpl extends ConstraintBase {
+class NoCostaintImpl extends NoConstraint {
 
   override def test(params: org.quantintel.ql.math.matrixutilities.Array): Boolean = true
 }
